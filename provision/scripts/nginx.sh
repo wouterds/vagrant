27ad66585c192a9c2a815aee87d7b8fac1892b89
WouterDS <wouter.de.schuyter@gmail.com>
2015-07-12 21:02:03 +0200

    Moved nginx config files to a subfolder

--- provision/scripts/nginx.sh
+++ provision/scripts/nginx.sh
@@ -4,11 +4,11 @@ echo "Installing Nginx.."
 apt-get install nginx -y
 
 echo "Configuring Nginx.."
-cp /vagrant/provision/config/nginx_vhost /etc/nginx/sites-available/nginx_vhost
+cp /vagrant/provision/config/nginx/nginx_vhost /etc/nginx/sites-available/nginx_vhost
 ln -s /etc/nginx/sites-available/nginx_vhost /etc/nginx/sites-enabled/
 rm -rf /etc/nginx/sites-available/default
 
-cp /vagrant/provision/config/www.my-project.dev /etc/nginx/sites-available/www.my-project.dev
+cp /vagrant/provision/config/nginx/www.my-project.dev /etc/nginx/sites-available/www.my-project.dev
 ln -s /etc/nginx/sites-available/www.my-project.dev /etc/nginx/sites-enabled/
 
 service nginx restart
